2245c49c0b707168fa57e44a6fb3688d971c361c
Gsantamaria88 <125360721+Gsantamaria88@users.noreply.github.com>
2025-07-15 13:19:27 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,21 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>3.0"
+    }
+  }
+}
+
 provider "azurerm" {
+  features = {}
+
+  tenant_id       = var.ARM_TENANT_ID
+  client_id       = var.ARM_CLIENT_ID
+  client_secret   = var.ARM_CLIENT_SECRET
+  subscription_id = var.ARM_SUBSCRIPTION_ID
+}
+
 variable "ARM_TENANT_ID" {
   description = "Azure Tenant ID"
   type        = string
@@ -17,7 +34,6 @@ variable "ARM_CLIENT_SECRET" {
 variable "ARM_SUBSCRIPTION_ID" {
   description = "Azure Subscription ID"
   type        = string
-  }
 }
 
 resource "azurerm_resource_group" "rg" {
