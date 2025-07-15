283ffffabc461a5ed8776d08f19c0fa977e1cba2
Gsantamaria88 <125360721+Gsantamaria88@users.noreply.github.com>
2025-07-15 13:31:15 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,31 +9,6 @@ terraform {
 
 provider "azurerm" {
   features = {}
-
-  tenant_id       = var.ARM_TENANT_ID
-  client_id       = var.ARM_CLIENT_ID
-  client_secret   = var.ARM_CLIENT_SECRET
-  subscription_id = var.ARM_SUBSCRIPTION_ID
-}
-
-variable "ARM_TENANT_ID" {
-  description = "Azure Tenant ID"
-  type        = string
-}
-
-variable "ARM_CLIENT_ID" {
-  description = "Azure Client ID"
-  type        = string
-}
-
-variable "ARM_CLIENT_SECRET" {
-  description = "Azure Client Secret"
-  type        = string
-}
-
-variable "ARM_SUBSCRIPTION_ID" {
-  description = "Azure Subscription ID"
-  type        = string
 }
 
 resource "azurerm_resource_group" "rg" {
