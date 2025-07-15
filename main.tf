90ae3be1e79b7f7f8ec044bc326f793def9d19fb
Gsantamaria88 <125360721+Gsantamaria88@users.noreply.github.com>
2025-07-15 12:51:25 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,3 +6,22 @@ resource "azurerm_resource_group" "rg" {
   name     = "rg-terraform-poc"
   location = "West Europe"
 }
+variable "ARM_TENANT_ID" {
+  description = "Azure Tenant ID"
+  type        = string
+}
+
+variable "ARM_CLIENT_ID" {
+  description = "Azure Client ID"
+  type        = string
+}
+
+variable "ARM_CLIENT_SECRET" {
+  description = "Azure Client Secret"
+  type        = string
+}
+
+variable "ARM_SUBSCRIPTION_ID" {
+  description = "Azure Subscription ID"
+  type        = string
+}
